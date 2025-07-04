8365918dee8fe3c7e206b7907d4be082feb1aa1b
Kamil Baldyga <baldygakamil@gmail.com>
2025-07-04 09:55:48 +0200

    create authRegister & authLogin reducers

--- src/lib/features/authSlice.jsx
+++ src/lib/features/authSlice.jsx
@@ -1,12 +1,68 @@
 const { createSlice } = require("@reduxjs/toolkit");
 
-const initialState = {};
-
+const initialState = {
+  users: [
+    {
+      name: "admin",
+      mail: "a@a.a",
+      password: "123",
+      games: [],
+      isConnected: false,
+    },
+  ],
+  user: {
+    name: "",
+    mail: "",
+    password: "",
+    games: [],
+    isConnected: false,
+  },
+  setMail: "",
+  setPassword: "",
+  setName: "",
+  isError: false,
+  error: {
+    register: "Cette adresse mail est déjà utilisée.",
+    login: "Adresse mail ou mot de passe invalide",
+  },
+};
 const AuthSlice = createSlice({
   name: "auth",
   initialState,
 
-  reducers: {},
+  reducers: {
+    authRegister: (state) => {
+      const user = state.users.find((u) => u.mail === state.setMail);
+      if (!user) {
+        state.users.push({
+          ...state.user,
+          name: state.setName,
+          mail: state.setMail,
+          password: state.setPassword,
+        });
+        state.isError = false;
+        state.setName = "";
+        state.setMail = "";
+        state.setPassword = "";
+      } else {
+        state.isError = true;
+      }
+    },
+    authLogin: (state) => {
+      const user = state.users.find(
+        (u) => u.mail === state.setMail && u.password === state.setPassword
+      );
+      if (user) {
+        state.user = { ...user, isConnected: true };
+        state.isError = false;
+        state.setMail = "";
+        state.setPassword = "";
+      } else {
+        state.isError = true;
+      }
+    },
+  },
 });
 
+export const { authRegister, authLogin } = AuthSlice.actions;
 export const AuthReducer = AuthSlice.reducer;
