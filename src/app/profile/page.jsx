6b8acf21b7ab4e7e9035a65c855c3eb667d7b295
Kamil Baldyga <baldygakamil@gmail.com>
2025-07-08 19:46:52 +0200

    add profile components

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,11 @@
+import ProfileInfo from "@/components/ProfileInfo/ProfileInfo";
+import ProfileOrders from "@/components/ProfileOrders/ProfileOrders";
+
+export default function Profile() {
+  return (
+    <>
+      <ProfileInfo />
+      <ProfileOrders />
+    </>
+  );
+}
