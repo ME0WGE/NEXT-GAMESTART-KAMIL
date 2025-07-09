a4d1bf344ef53d13194d5b0922e0932804afac3e
Kamil Baldyga <baldygakamil@gmail.com>
2025-07-09 13:53:30 +0200

    Change profile button colors

--- src/components/ProfileInfo/ProfileInfo.jsx
+++ src/components/ProfileInfo/ProfileInfo.jsx
@@ -3,12 +3,12 @@ export default function ProfileInfo() {
     <section className="profile-info">
       <ul className="profile-info-list space-y-3">
         <li className="flex justify-between text-neutral-300">
-          <button className="bg-neutral-800 text-neutral-100 px-4 py-2 rounded-md hover:bg-neutral-700 transition-colors duration-300 text-center font-mono text-sm hover:text-yellow-400 hover:border-yellow-400 border-2 border-white w-35 cursor-pointer">
+          <button className="bg-neutral-800 text-neutral-100 px-4 py-2 rounded-md hover:bg-plum/10 transition-colors duration-200 text-center font-mono text-sm hover:text-pine hover:border-rosy border-2 border-white w-35 cursor-pointer">
             Edit Profile
           </button>
         </li>
         <li className="flex justify-between text-neutral-300">
-          <button className="bg-neutral-800 text-neutral-100 px-4 py-2 rounded-md hover:bg-neutral-700 transition-colors duration-300 text-center font-mono text-sm hover:text-yellow-400 hover:border-yellow-400 border-2 border-white w-35 cursor-pointer">
+          <button className="bg-neutral-800 text-neutral-100 px-4 py-2 rounded-md hover:bg-plum/20 transition-colors duration-200 text-center font-mono text-sm hover:text-pine hover:border-rosy border-2 border-white w-35 cursor-pointer">
             Add Credits
           </button>
         </li>
