424bdb2b920e5b93664415691ff29773c2c66c67
Kamil Baldyga <baldygakamil@gmail.com>
2025-07-15 09:28:48 +0200

    feat(GameDetails): Add routing for game & gameId pages.

---
+++ src/app/game/page.jsx
@@ -0,0 +1,12 @@
+export default function Game() {
+  return (
+    <>
+      <div className="pt-20">
+        <h1>
+          Découvrez nos promotions LUNAIRES et notre sélection des jeux vidéos!
+        </h1>
+        <button>Cliquez-moi!</button>
+      </div>
+    </>
+  );
+}
